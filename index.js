51f534ebc8390e4bc10f61d58f83cfbd8124d79b
1839900603142 <624235034@parichat.skru.ac.th>
2021-08-13 16:50:25 +0700

    add plus and getbook by id

--- index.js
+++ index.js
@@ -38,6 +38,36 @@ app.get('/books',  function (req, res)  {
   
 });
 
+app.get('/students',  function (req, res)  {  
+
+	res.setHeader('Content-Type', 'application/json');
+
+	var booksReference = db.ref("students");
+
+	//Attach an asynchronous callback to read the data
+	booksReference.on("value", 
+				function(snapshot) {					
+					res.json(snapshot.val());
+					booksReference.off("value");
+					}, 
+				function (errorObject) {
+					res.send("The read failed: " + errorObject.code);
+				});
+  
+});
+
+
+app.post('/plus',  function (req, res){
+	res.setHeader('Content-Type', 'application/json');
+
+	var num1 = req.body.num1;
+	var num2 = req.body.num2;
+
+	res.send('{ "result": ' + (num1+num2) + '}');
+
+
+});
+
 
 app.get('/topsellers',  function (req, res)  {  
 
@@ -59,10 +89,26 @@ app.get('/topsellers',  function (req, res)  {
 
 
 app.get('/book/:bookid',  function (req, res)  {  
-  	
-		//Code Here
+	res.setHeader('Content-Type', 'application/json');
+	var bookid = Number(req.params.bookid);
 
+	var booksReference = db.ref("books");
+
+	//Attach an asynchronous callback to read the data
+	booksReference.orderByChild("bookid").equalTo(bookid).on("child_added", 
+				function(snapshot) {					
+					res.json(snapshot.val());
+					booksReference.off("value");
+					}, 
+				function (errorObject) {
+					res.send("The read failed: " + errorObject.code);
+				});
+  
 });
+
+
+
+		//Code Here
 
 app.delete('/book/:bookid',  function (req, res)  {  
   	
